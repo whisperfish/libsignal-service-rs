f83130594abe90421861fd003b668891ed9cba22
Michael Bryan <michaelfbryan@gmail.com>
2019-06-06 22:38:25 +0800

    Stubbed out part of the registering example

---
+++ libsignal-service/examples/registering.rs
@@ -0,0 +1,88 @@
+//! At install time, clients need to register with the Signal server.
+//!
+//! ```java
+//! private final String     URL         = "https://my.signal.server.com";
+//! private final TrustStore TRUST_STORE = new MyTrustStoreImpl();
+//! private final String     USERNAME    = "+14151231234";
+//! private final String     PASSWORD    = generateRandomPassword();
+//! private final String     USER_AGENT  = "[FILL_IN]";
+//!
+//! SignalServiceAccountManager accountManager = new SignalServiceAccountManager(URL, TRUST_STORE,
+//!                                                                              USERNAME, PASSWORD, USER_AGENT);
+//!
+//! accountManager.requestSmsVerificationCode();
+//! accountManager.verifyAccountWithCode(receivedSmsVerificationCode, generateRandomSignalingKey(),
+//!                                      generateRandomInstallId(), false);
+//! accountManager.setGcmId(Optional.of(GoogleCloudMessaging.getInstance(this).register(REGISTRATION_ID)));
+//! accountManager.setPreKeys(identityKey.getPublicKey(), lastResortKey, signedPreKeyRecord, oneTimePreKeys);
+//! ```
+
+use failure::Error;
+use libsignal_service::{AccountManager, TrustStore};
+use std::io;
+use structopt::StructOpt;
+
+fn main() -> Result<(), Error> {
+    let args = Args::from_args();
+
+    let trust_store = args.load_trust_store()?;
+    let password = args.get_password()?;
+
+    let mut account_manager = AccountManager::builder()
+        .server(args.url)
+        .credentials(args.username, password)
+        .trust_store(trust_store)
+        .user_agent(args.user_agent)
+        .build();
+
+    account_manager.request_sms_verification_code()?;
+
+    Ok(())
+}
+
+#[derive(Debug, Clone, PartialEq, StructOpt)]
+pub struct Args {
+    #[structopt(
+        short = "s",
+        long = "server",
+        help = "The server to connect to",
+        default_value = "https://my.signal.server.com"
+    )]
+    pub url: String,
+    #[structopt(
+        short = "u",
+        long = "username",
+        help = "Your username or other identifier",
+        default_value = "+14151231234"
+    )]
+    pub username: String,
+    #[structopt(
+        short = "p",
+        long = "password",
+        help = "The password to use. Read from stdin if not provided"
+    )]
+    pub password: Option<String>,
+    #[structopt(
+        long = "user-agent",
+        help = "The user agent to use when contacting servers",
+        raw(default_value = "libsignal_service::USER_AGENT")
+    )]
+    pub user_agent: String,
+}
+
+impl Args {
+    pub fn load_trust_store(&self) -> Result<TrustStore, Error> {
+        Ok(TrustStore)
+    }
+
+    pub fn get_password(&self) -> Result<String, Error> {
+        if let Some(ref pw) = self.password {
+            return Ok(pw.clone());
+        }
+
+        let mut line = String::new();
+        io::stdin().read_line(&mut line)?;
+
+        Ok(line.trim().to_string())
+    }
+}
