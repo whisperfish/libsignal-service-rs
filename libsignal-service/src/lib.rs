f54066da8b2af306838c10c5e4e639018ca2ad26
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-07-14 18:00:00 +0200

    rustfmt src/lib

--- libsignal-service/src/lib.rs
+++ libsignal-service/src/lib.rs
@@ -17,7 +17,8 @@ pub use crate::account_manager::AccountManager;
 pub const USER_AGENT: &'static str =
     concat!(env!("CARGO_PKG_NAME"), "-rs-", env!("CARGO_PKG_VERSION"));
 
-/// GROUP_UPDATE_FLAG signals that this message updates the group membership or name.
+/// GROUP_UPDATE_FLAG signals that this message updates the group membership or
+/// name.
 pub const GROUP_UPDATE_FLAG: u32 = 1;
 
 /// GROUP_LEAVE_FLAG signals that this message is a group leave message.
