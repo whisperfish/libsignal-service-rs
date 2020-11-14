2e59a28dd809e0d455dfc731446224358c00d236
rubdos <ruben.de.smet@rubdos.be>
2020-11-14 20:12:53 +0000

    Deploying to gh-pages from  @ e1e59ef5099bbf4a1e20ecc86e192be5c9664e43 🚀

--- libsignal_service/cipher/sidebar-items.js
+++ libsignal_service/cipher/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ServiceCipher","Decrypts incoming messages and encrypts outgoing messages."]]});
+initSidebarItems({"fn":[["get_preferred_protocol_address","Equivalent of `SignalServiceCipher::getPreferredProtocolAddress`"]],"struct":[["ServiceCipher","Decrypts incoming messages and encrypts outgoing messages."]]});
---
+++ libsignal_service/sealed_session_cipher/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["MacError",""],["SealedSessionError",""]],"struct":[["CertificateValidator",""],["SenderCertificate",""],["ServerCertificate",""],["UnidentifiedAccess",""],["UnidentifiedAccessPair",""],["UnidentifiedSenderMessageContent",""]]});
