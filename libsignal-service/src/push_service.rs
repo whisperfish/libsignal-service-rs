f86f31cd7a08c1c445269b0c6fb7d49d7289c030
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-01-23 10:18:22 +0100

    PushService::linked_devices

--- libsignal-service/src/push_service.rs
+++ libsignal-service/src/push_service.rs
@@ -370,6 +370,17 @@ pub trait PushService {
         Ok(VoiceVerificationCodeResponse::CallIssued)
     }
 
+    async fn devices(&mut self) -> Result<Vec<DeviceInfo>, ServiceError> {
+        #[derive(serde::Deserialize)]
+        struct DeviceInfoList {
+            devices: Vec<DeviceInfo>,
+        }
+
+        let devices: DeviceInfoList = self.get("/v1/devices/").await?;
+
+        Ok(devices.devices)
+    }
+
     async fn confirm_verification_code(
         &mut self,
         confirm_code: u32,
