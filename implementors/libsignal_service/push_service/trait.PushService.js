7c1fee12440f3d79fd027c7e2c8e95cb3a54dec6
gferon <g@leirbag.net>
2021-03-08 19:10:49 +0000

    Deploying to gh-pages from  @ 3e5d4951492fa51b3a6a8049531e8b4646042550 🚀

--- implementors/libsignal_service/push_service/trait.PushService.js
+++ implementors/libsignal_service/push_service/trait.PushService.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["libsignal_service_actix"] = [{"text":"impl PushService for AwcPushService","synthetic":false,"types":[]}];
+implementors["libsignal_service_hyper"] = [{"text":"impl PushService for HyperPushService","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
