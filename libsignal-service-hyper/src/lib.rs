3961f076bc92c3abcf6ddd3de471e852ae6fa64c
Schmiddiii <github@schmiddi.anonaddy.com>
2022-09-20 19:40:27 +0200

    Throw a compilation error when hyper is compiled with unsend-futures
    This throws a descriptive compilation error when libsignal-service-hyper
    is compiled with unsend-futures like discussed in the Matrix.

--- libsignal-service-hyper/src/lib.rs
+++ libsignal-service-hyper/src/lib.rs
@@ -1,5 +1,8 @@
 #![recursion_limit = "256"]
 
+#[cfg(feature = "unsend-futures")]
+compile_error!("`libsignal-service-hyper` cannot be compiled with the feature `unsend-futures` from `libsignal-service`.");
+
 pub mod push_service;
 pub mod websocket;
 
