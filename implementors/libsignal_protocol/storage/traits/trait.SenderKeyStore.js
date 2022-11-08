4472271fcfd7bd3198eb93b10fb91efabdabb533
gferon <g@leirbag.net>
2022-11-08 09:01:43 +0000

    Deploying to gh-pages from  @ 0e851d867a33e74008335a4174da8a8c47faba33 🚀

--- implementors/libsignal_protocol/storage/traits/trait.SenderKeyStore.js
+++ implementors/libsignal_protocol/storage/traits/trait.SenderKeyStore.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {};
 implementors["libsignal_protocol"] = [];
+implementors["libsignal_service"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
