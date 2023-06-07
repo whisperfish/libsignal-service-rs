e5ba72d86f4ee2b2e355c670f5c0a84492db29dc
R-D-C-N <57043256+R-D-C-N@users.noreply.github.com>
2023-06-07 12:15:08 -0400

    Add api for retrieving stickerpacks from the cdn

--- libsignal-service/src/push_service.rs
+++ libsignal-service/src/push_service.rs
@@ -592,6 +592,23 @@ pub trait PushService: MaybeSend {
         }
     }
 
+    async fn get_sticker_pack_manifest(
+        &mut self,
+        id: &str,
+    ) -> Result<Self::ByteStream, ServiceError> {
+        let path = format!("/stickers/{}/manifest.proto", id);
+        self.get_from_cdn(0, &path).await
+    }
+
+    async fn get_sticker(
+        &mut self,
+        pack_id: &str,
+        sticker_id: u32,
+    ) -> Result<Self::ByteStream, ServiceError> {
+        let path = format!("/stickers/{}/full/{}", pack_id, sticker_id);
+        self.get_from_cdn(0, &path).await
+    }
+
     async fn send_messages(
         &mut self,
         messages: OutgoingPushMessages,
