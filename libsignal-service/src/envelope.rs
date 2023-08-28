8c8cae28aadbd4eeefc1d6eb0b2f14c86fefaba5
Ruben De Smet <ruben.de.smet@rubdos.be>
2023-08-28 15:28:23 +0200

    Add a bunch of new EnvelopeEntity fields

--- libsignal-service/src/envelope.rs
+++ libsignal-service/src/envelope.rs
@@ -139,10 +139,22 @@ pub struct EnvelopeEntity {
     pub source: Option<String>,
     pub source_uuid: Option<String>,
     pub source_device: u32,
+    #[serde(default)]
+    pub destination_uuid: Option<String>,
     #[serde(default, with = "serde_optional_base64")]
     pub content: Option<Vec<u8>>,
     pub server_timestamp: u64,
     pub guid: String,
+    #[serde(default = "default_true")]
+    pub urgent: bool,
+    #[serde(default)]
+    pub story: bool,
+    #[serde(default, with = "serde_optional_base64")]
+    pub report_spam_token: Option<Vec<u8>>,
+}
+
+fn default_true() -> bool {
+    true
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
