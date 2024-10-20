0a0d37c097844003239c27a00dfc7c182308a66b
Gabriel Féron <g@leirbag.net>
2024-10-20 20:06:42 +0200

    Add CDN3 in staging

--- src/configuration.rs
+++ src/configuration.rs
@@ -190,6 +190,7 @@ impl From<&SignalServers> for ServiceConfiguration {
                     let mut map = HashMap::new();
                     map.insert(0, "https://cdn-staging.signal.org".parse().unwrap());
                     map.insert(2, "https://cdn2-staging.signal.org".parse().unwrap());
+                    map.insert(3, "https://cdn3-staging.signal.org".parse().unwrap());
                     map
                 },
                 contact_discovery_url:
