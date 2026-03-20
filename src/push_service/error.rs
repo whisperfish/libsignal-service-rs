76c4cd0d9ad095b9c6ddd583cbf8c23bdb60505d
Ruben De Smet <ruben.de.smet@rubdos.be>
2026-03-20 16:18:58 +0100

    feat: expose USMC content when sealed sender decryption fails

--- src/push_service/error.rs
+++ src/push_service/error.rs
@@ -5,7 +5,9 @@ use libsignal_protocol::{
 };
 use zkgroup::{ZkGroupDeserializationFailure, ZkGroupVerificationFailure};
 
-use crate::groups_v2::GroupDecodingError;
+use crate::{
+    cipher::SealedSenderDecryptionError, groups_v2::GroupDecodingError,
+};
 
 use super::{
     MismatchedDevices, ProofRequired, RegistrationLockFailure, StaleDevices,
@@ -67,6 +69,9 @@ pub enum ServiceError {
 
     #[error("Protocol error: {0}")]
     SignalProtocolError(#[from] SignalProtocolError),
+
+    #[error("Sealed sender decryption error: {0}")]
+    SealedSenderDecryptionError(#[from] SealedSenderDecryptionError),
 
     #[error("invalid device id: {0}")]
     InvalidDeviceId(#[from] libsignal_core::InvalidDeviceId),
